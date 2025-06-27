c84f432caa1410dd7e8454820be5fbae1814a575
rohitrajpurohit17 <rrajpurohit9673@gmail.com>
2025-06-27 16:29:51 +0530

    Added factFunn

--- 01Basic/SumFun.java
+++ 01Basic/SumFun.java
@@ -8,14 +8,27 @@ public class SumFun {
         double result= num1 * num2;
         return result;
     }
+    public static void factNumber(int num){
+        int fact = 1;
+        if(num < 0){
+            System.out.println("invalid number");
+            return;
+        }
+        for(int i = 1; i <= num ; i++){
+           fact *= i;
+        }
+        System.out.println(fact);
+        return;
+    }
     public static void main(String[] args) {
         double num1 = 10;
         double num2 = 20;
-
+        int num = -1;
         // double answer=addNumbers(num1,num2);
 
-        double answer=multiNumbers(num1,num2);
-        System.out.println(answer);
+        // double answer=multiNumbers(num1,num2);
+        factNumber(num);
+      
 
     }
 }
