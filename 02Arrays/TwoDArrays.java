7cd059a61dbccac766fdf4e71d877ef728ebc794
rohitrajpurohit17 <rrajpurohit9673@gmail.com>
2025-07-12 13:23:42 +0530

    JaggedArray

--- 02Arrays/TwoDArrays.java
+++ 02Arrays/TwoDArrays.java
@@ -1,19 +1,21 @@
-class TwoDArrays{
+
+class TwoDArrays {
+
     public static void main(String[] args) {
         int[][] num = new int[3][4];
 
-        for(int i =0; i < 3; i++){
-            for(int j = 0; j < 4; j++){
-                num[i][j] = (int)(Math.random() * 1000);
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 4; j++) {
+                num[i][j] = (int) (Math.random() * 1000);
             }
         }
 
-    // Adance way to print then our regular wway 
-    for(int n[] :num){
-        for(int m : n){
-            System.out.print(m + " " );
-        }
+        // Adance way to print then our regular wway 
+        for (int n[] : num) {
+            for (int m : n) {
+                System.out.print(m + " ");
+            }
             System.out.println();
-    }
+        }
     }
 }
