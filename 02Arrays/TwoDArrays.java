6c414a59c3f70f8abfb518beb11d49572ca745d7
rohitrajpurohit17 <rrajpurohit9673@gmail.com>
2025-07-12 12:34:35 +0530

    Added 2D Araay

---
+++ 02Arrays/TwoDArrays.java
@@ -0,0 +1,19 @@
+class TwoDArrays{
+    public static void main(String[] args) {
+        int[][] num = new int[3][4];
+
+        for(int i =0; i < 3; i++){
+            for(int j = 0; j < 4; j++){
+                num[i][j] = (int)(Math.random() * 1000);
+            }
+        }
+
+    // Adance way to print then our regular wway 
+    for(int n[] :num){
+        for(int m : n){
+            System.out.print(m + " " );
+        }
+            System.out.println();
+    }
+    }
+}
